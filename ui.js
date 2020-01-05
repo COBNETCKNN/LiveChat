3c5f349bb1b3e58d199121fc7483da7b8fb4cd98
Ahmed Meškin <57231096+COBNETCKNN@users.noreply.github.com>
2020-01-05 01:46:03 +0100

    Add files via upload

---
+++ ui.js
@@ -0,0 +1,22 @@
+// render chat template to the DOM
+// clear the list of the chats (when the room changes)
+
+class ChatUI {
+    constructor(list){
+        this.list = list;
+    }
+    clear(){
+        this.list.innerHTML = '';
+    }
+    render(data){
+        const html = `
+            <li class="list-group-item">
+                <span class="username">${data.username}</span>
+                <span class="message">${data.message}</span>
+                <div class="time">${data.created_at.toDate()}</div>
+            </li>
+        `;
+
+        this.list.innerHTML += html;
+    }
+}
